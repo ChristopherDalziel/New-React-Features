d51414da0179c5c824e80a489adb41255fc72a8b
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-06-07 14:40:06 +1000

    implimented a basic counter using the useState function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 
+// Hooks are nothing more than functions
+
+const App = (props) => {
+  // count stores the count value, and setCount because it's setting the count. However these names can be anything.
+  const [count, setCount] = useState(10);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(props.count);
+  };
+
+  return (
+    <div>
+      <p>The current count is ${count}</p>
+      <button onClick={increment}>+ 1</button>
+      <button onClick={decrement}>- 1</button>
+      <button onClick={reset}>reset</button>
+    </div>
+  );
+};
+
+// Setting the default of count to 0 if an amount is not added to the 'App' props.
+App.defaultProps = {
+  count: 0,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <div>my new content</div>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
